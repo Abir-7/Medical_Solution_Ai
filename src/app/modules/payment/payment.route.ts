d827a9416538ceb1c6a9fb9db19e1b2a23f5bcb0
Abir-7 <md.tazwarul.islam.07@gmail.com>
2025-06-28 14:31:15 +0600

    feat: add payment routes for creating payment intent and webhook handling

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
 import { PaymentController } from "./payment.controller";
+import { auth } from "../../middlewares/auth/auth";
 const router = Router();
+
+router.get(
+  "/new/:tokenPackageId",
+  auth("USER"),
+  PaymentController.createPaymentIntent
+);
 
 router.post(
   "/webhook",
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { AuthRoute } from "../modules/auth/auth.route";
 import { UserProfileRoute } from "../modules/users/userProfile/userProfile.route";
 import { MedicalReportRoute } from "../modules/medicalReport/medicalReport.route";
 import { TokenPackageRoute } from "../modules/TokenPackages/tokenPackages.route";
+import { PaymentRoute } from "../modules/payment/payment.route";
 
 const router = Router();
 const apiRoutes = [
@@ -12,6 +13,7 @@ const apiRoutes = [
   { path: "/auth", route: AuthRoute },
   { path: "/ai", route: MedicalReportRoute },
   { path: "/token-package", route: TokenPackageRoute },
+  { path: "/payment", route: PaymentRoute },
 ];
 apiRoutes.forEach((route) => router.use(route.path, route.route));
 export default router;
