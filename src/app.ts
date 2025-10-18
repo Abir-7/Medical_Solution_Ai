081255a214e303d72876cee1ff2a72e394752058
Abir-7 <md.tazwarul.islam.07@gmail.com>
2025-10-18 18:15:22 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
 
 app.use("/api", router);
 
